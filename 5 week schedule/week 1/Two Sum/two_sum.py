923e96411e58ec97f54d4f82928aa54a3b694881
Aashish Chandra Bivek <aashish.2chandra@gmail.com>
2022-05-02 00:15:33 +0545

    two sum solution

---
+++ 5 week schedule/week 1/Two Sum/two_sum.py
@@ -0,0 +1,19 @@
+class Solution:
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def two_sum(nums, target):
+        for index in range(0, len(nums)-1):
+            key = target - nums[index]
+            if key in nums[index+1:]:
+                if key != nums[index]:
+                    return [index, nums.index(key)]
+                else:
+                    return [index, nums[index+1:].index(key) + index + 1]
+
+
+print(Solution().two_sum([3, 2, 4], 6))
+print(Solution().two_sum([3, 3], 6))
+print(Solution().two_sum([2, 5, 5, 7], 10))
