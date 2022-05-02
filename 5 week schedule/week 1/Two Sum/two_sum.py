7fe811070d59435d76c97638b6c6410f5767b5f7
Aashish Chandra Bivek <aashish.2chandra@gmail.com>
2022-05-02 15:51:33 +0545

    improvement: remove unnecessary if-else

--- 5 week schedule/week 1/Two Sum/two_sum.py
+++ 5 week schedule/week 1/Two Sum/two_sum.py
@@ -8,10 +8,7 @@ class Solution:
         for index in range(0, len(nums)-1):
             key = target - nums[index]
             if key in nums[index+1:]:
-                if key != nums[index]:
-                    return [index, nums.index(key)]
-                else:
-                    return [index, nums[index+1:].index(key) + index + 1]
+                return [index, nums[index+1:].index(key) + index + 1]
 
 
 print(Solution().two_sum([3, 2, 4], 6))
